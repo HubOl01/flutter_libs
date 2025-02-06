a6cf93457e036c6e3ae19502ac57331ff70917c6
Oleg Kulichkov <kuloleg52@gmail.com>
2025-02-06 23:39:40 +0300

    Update favoriteModel.dart

--- core/models/favoriteModel.dart
+++ core/models/favoriteModel.dart
@@ -1,31 +1,36 @@
 const String tableName = "favorite";
 
 class Favorites {
-  static final List<String> values = [id, title, isFavorite];
+  static final List<String> values = [id, title, isFavorite, createdAt];
   static const String id = "id";
   static const String title = "title";
   static const String isFavorite = "isFavorite";
+  static const String createdAt = "createdAt";
 }
 
 class FavoriteModel {
   final int? id;
   final String title;
   final int isFavorite;
+  final DateTime createdAt;
 
   FavoriteModel({
     this.id,
     required this.title,
     this.isFavorite = 0,
+    required this.createdAt,
   });
   FavoriteModel copyWith({
     int? id,
     String? title,
     int? isFavorite,
+    DateTime? createdAt,
   }) {
     return FavoriteModel(
       id: id ?? this.id,
       title: title ?? this.title,
       isFavorite: isFavorite ?? this.isFavorite,
+      createdAt: createdAt ?? this.createdAt,
     );
   }
 
@@ -34,6 +39,7 @@ class FavoriteModel {
       'id': id,
       'title': title,
       'isFavorite': isFavorite,
+      'createdAt': createdAt.toIso8601String(),
     };
   }
 
@@ -42,6 +48,7 @@ class FavoriteModel {
       id: map['id'],
       title: map['title'],
       isFavorite: map['isFavorite'],
+      createdAt: DateTime.parse(map['createdAt'] as String)
     );
   }
 }
